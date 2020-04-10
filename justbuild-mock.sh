a9f097397b0b1a4751de23c4f7659e4a36b32341
Ron Olson <tachoknight@gmail.com>
2020-04-10 09:15:13 -0500

    Modified mock shell script

--- justbuild-mock.sh
+++ justbuild-mock.sh
@@ -9,13 +9,12 @@ rm -rf $MYDIR/mock-results
 mkdir $MYDIR/mock-results
 mkdir -p $HOME/rpmbuild/{BUILD,RPMS,SOURCES,SPECS,SRPMS}
 cp $PWD/*.patch $HOME/rpmbuild/SOURCES
-cp $PWD/*.conf $HOME/rpmbuild/SOURCES
 cp $PWD/swift-lang.spec $HOME/rpmbuild/SPECS
 
 pushd $HOME/rpmbuild/SPECS
 spectool -g -R ./swift-lang.spec
 # Now do the actual build
-mock --clean -r fedora-rawhide-x86_64 --spec=swift-lang.spec --sources=../SOURCES --resultdir=$MYDIR/mock-results --buildsrpm --rebuild --rpmbuild-opts=--noclean --no-cleanup-after 2>&1 | tee $MYDIR/mock-results/build-output.txt
+mock --clean -r fedora-30-x86_64 --spec=swift-lang.spec --sources=../SOURCES --resultdir=$MYDIR/mock-results --buildsrpm --rebuild --rpmbuild-opts=--noclean --no-cleanup-after 2>&1 | tee $MYDIR/mock-results/build-output.txt
 popd
 
 echo Started:_____$START_TS
