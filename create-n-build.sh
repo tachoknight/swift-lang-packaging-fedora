810fa8097615b11848c26660ce161053d82ce55c
Ron Olson <tachoknight@gmail.com>
2021-06-14 10:54:59 -0500

    Recreating the check-n-build process

---
+++ create-n-build.sh
@@ -0,0 +1,55 @@
+#!/bin/bash
+
+THISDIR=$HOME/swift-lang-packaging-fedora
+
+pushd $THISDIR
+
+# Gives us the version number of this platform
+THISVERSION=`cat /etc/os-release | grep PLATFORM_ID | awk -F: '/platform:*/{print $2}' | cut -c 2- | rev | cut -c 2- | rev`
+echo Building on $THISVERSION of Fedora
+
+# First we want to capture the hash of the swift-lang.spec
+# file...
+bh=`md5sum ./swift-lang.spec`
+
+# Now run the python program to check for updates
+# and modify the swift-lang.spec file accordingly
+./nrc.py
+
+# Now hash the file again
+ah=`md5sum ./swift-lang.spec`
+
+# And now only bother doing the rest of the script
+# if the hash was changed, otherwise exit
+if [ "$bh" = "$ah" ]; then
+	exit 0
+fi
+
+# Okay, we're gonna do this...
+
+rm -rf /home/rolson/rpmbuild
+rm $THISDIR/cnb-build-output.txt
+mkdir -p /home/rolson/rpmbuild/{BUILD,RPMS,SOURCES,SPECS,SRPMS}
+cp $THISDIR/*.patch /home/rolson/rpmbuild/SOURCES
+cp $THISDIR/*.conf /home/rolson/rpmbuild/SOURCES
+cp $THISDIR/swift-lang.spec /home/rolson/rpmbuild/SPECS
+
+pushd /home/rolson/rpmbuild/SPECS
+spectool -g -R ./swift-lang.spec
+# Get the dependencies
+#dnf builddep -y ./swift-lang.spec
+# Now do the actual build
+rpmbuild -ba ./swift-lang.spec 2>&1 | tee $THISDIR/cnb-build-output.txt
+popd
+
+# And commit it to the nightly-builds branch
+#git checkout nightly-builds
+git commit -am "Updated to `awk '/%global swifttag *./{print $3}' ./swift-lang.spec`"
+git push 
+
+# Now move it to fedorapeople
+ssh fedorapeople.org "rm ~/public_html/swift-lang/*$THISVERSION*.rpm"
+scp /home/rolson/rpmbuild/SRPMS/* fedorapeople.org:~/public_html/swift-lang
+scp /home/rolson/rpmbuild/RPMS/x86_64/* fedorapeople.org:~/public_html/swift-lang
+scp /home/rolson/rpmbuild/SPECS/* fedorapeople.org:~/public_html/swift-lang
+
---
+++ nrc.py
@@ -0,0 +1,108 @@
+#!/usr/bin/python3
+
+import feedparser
+import re
+import datetime
+import requests
+from bs4 import BeautifulSoup
+
+# The version we're autobuilding
+CURRENT_VERSION = 'swift-5.5'
+
+def left(s, amount):
+    return s[:amount]
+
+def right(s, amount):
+    return s[-amount:]
+
+def mid(s, offset, amount):
+    return s[offset:offset+amount]
+
+def getDate(s):
+    # Because we know the format of the string, this is safe to do
+    # (e.g. swift-4.2-DEVELOPMENT-SNAPSHOT-2018-07-17-a)
+    return datetime.datetime.strptime(mid(s.strip(), 31, len(s.strip())-31-2), '%Y-%m-%d').date()
+
+def getGitTag(post):
+    f = requests.get(post.link)
+    soup = BeautifulSoup(f.content, 'html.parser')
+    elems = soup.code
+    return [elem.string for elem in elems][0]
+
+def getSpecFileContents():
+    with open('swift-lang.spec', 'r') as f:
+         return f.read()
+
+def changeData(f, textToFind, textToReplace):
+    p = re.compile(textToFind)
+    s = p.search(f)
+    t = s.group() # This is the line from the file we want to replace
+    # And replace it
+    return f.replace(t, textToReplace)
+
+def changePackageNumber(f):
+    p = re.compile('Release: .*')
+    s = p.search(f)
+    t = s.group()
+    
+    # Now we need to get the package number
+    p2 = re.compile('[0-9].[0-9].')
+    s2 = p2.search(t)
+    t2 = s2.group()
+
+    # Now we have our number, so we want to increment that
+    newPN = int(t2.split('.')[1]) + 1
+
+    # And we need to return the fixed line, as well as the new package number so we can use it for
+    # the changelog
+    return f.replace(t, 'Release:        0.' + str(newPN) + '.%{swiftgitdate}git%{swiftgithash}%{?dist}'), newPN
+
+def process(post, postDate):
+    print("Going to work with " + post.title + " from " + post.link)
+    gitHash = getGitTag(post)
+    print("The git hash is " + gitHash)
+    # We need the spec file contents 
+    spec = getSpecFileContents()
+    #
+    # Now let's fiddle with the file
+    #
+
+    # First change the file date
+    newTitle = post.title
+    newTitle = newTitle.replace('swift-', '')
+    spec = changeData(spec, 'swifttag .*', 'swifttag ' + newTitle) 
+
+    nf = open('swift-lang.spec', 'w')
+    nf.write(spec)
+    nf.close()
+
+    # And write out our last-release file
+    nlrf = open('last-release.txt', 'w')
+    nlrf.write(post.title)
+    nlrf.close()
+
+
+lastBuild=''
+with open('last-release.txt', 'r') as lastbuildfile:
+    lastBuild=lastbuildfile.read().replace('\n', '')
+
+print('Last build was ', lastBuild)
+lastDate = getDate(lastBuild)
+
+d = feedparser.parse("https://github.com/apple/swift/releases.atom")
+
+# We're gonna start from the top as that's the latest one
+
+print("Ok, Gonna go through them...")
+for post in d.entries:    
+    print(post.title)
+    if left(post.title, 9) == CURRENT_VERSION:        
+        postDate = getDate(post.title)
+        # Okay, is this date newer than the last time we
+        # processed anything?
+        if postDate > lastDate:
+            print("yep, got to do it!")
+            process(post, postDate)
+            break
+
+
