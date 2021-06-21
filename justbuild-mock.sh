7b4a9870fa1cb965f8558ac9c92c68a4ace3dd8f
Ron Olson <tachoknight@gmail.com>
2021-06-21 13:42:51 -0500

    Made change to test building epel 8 with mock

--- justbuild-mock.sh
+++ justbuild-mock.sh
@@ -4,10 +4,11 @@ MYDIR=$PWD
 
 START_TS=`date`
 
-BUILD=fedora-rawhide-x86_64
-#BUILD=epel-8-x86_64
+#BUILD=fedora-rawhide-x86_64
+BUILD=epel-8-x86_64
 #BUILD=epelplayground-8-x86_64
 #BUILD=fedora-35-x86_64
+#BUILD=fedora-33-x86_64
 
 rm -rf $HOME/rpmbuild
 rm -rf $MYDIR/mock-results
@@ -24,7 +25,8 @@ echo Now getting the sources...
 spectool -g -R ./swift-lang.spec
 # Now do the actual build
 echo Now gonna hopefully build it
-mock --clean -r $BUILD --enablerepo=local --spec=swift-lang.spec --sources=../SOURCES --resultdir=$MYDIR/mock-results --buildsrpm --rebuild --rpmbuild-opts=--noclean --no-cleanup-after 2>&1 | tee $MYDIR/mock-results/build-output.txt
+#mock --clean -r $BUILD --enablerepo=local --spec=swift-lang.spec --sources=../SOURCES --resultdir=$MYDIR/mock-results --buildsrpm --rebuild --rpmbuild-opts=--noclean --no-cleanup-after 2>&1 | tee $MYDIR/mock-results/build-output.txt
+mock --clean -r $BUILD --spec=swift-lang.spec --sources=../SOURCES --resultdir=$MYDIR/mock-results --buildsrpm --rebuild --rpmbuild-opts=--noclean --no-cleanup-after 2>&1 | tee $MYDIR/mock-results/build-output.txt
 popd
 
 echo Started:_____$START_TS
