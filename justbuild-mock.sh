15d75d4c30010e746a6bdd0253718f23f01739f4
Ron Olson <tachoknight@gmail.com>
2022-07-13 11:04:44 -0500

    Changed to rawhide

--- justbuild-mock.sh
+++ justbuild-mock.sh
@@ -4,8 +4,8 @@ MYDIR=$PWD
 
 START_TS=`date`
 
-#BUILD=fedora-rawhide-`uname -i`
-BUILD=fedora-36-`uname -i`
+BUILD=fedora-rawhide-`uname -i`
+#BUILD=fedora-36-`uname -i`
 
 rm -rf $HOME/rpmbuild
 rm -rf $MYDIR/mock-results
