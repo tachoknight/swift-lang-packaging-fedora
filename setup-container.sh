1fd866356207012fe03a26066b165416be079208
Ron Olson <tachoknight@gmail.com>
2022-09-27 13:23:38 -0500

    Added special case for oracle

--- setup-container.sh
+++ setup-container.sh
@@ -7,10 +7,21 @@
 # Also note that this is specific to Fedora-flavored containers.
 #
 
+# Which flavor are we using, because some require different things
+FLAVOR=`( lsb_release -ds || cat /etc/*release || uname -om ) 2>/dev/null | head -n1`
+echo Working with $FLAVOR
+
 dnf install -y rpm-build rpm-devel rpmdevtools vim epel-release
 dnf install -y 'dnf-command(config-manager)'
 dnf config-manager --set-enabled epel
-dnf config-manager --set-enabled powertools
+
+# Oracle does it differently
+if [[ $FLAVOR == "Oracle"* ]]; then
+	dnf config-manager --set-enabled ol8_codeready_builder
+else
+	dnf config-manager --set-enabled powertools
+fi
+
 # Now get the dependencies from the spec file
 dnf builddep -y ./swift-lang.spec
 
