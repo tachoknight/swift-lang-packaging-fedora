c5ecc304b90d2b0a288b291381b4c391ba2849b2
Ron Olson <tachoknight@gmail.com>
2022-09-27 11:17:34 -0500

    Added script to make containers easier to prepare for building

---
+++ setup-container.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+#
+# The purpose of this script is to prepare a container environment
+# to build Swift. Note that this script assumes you are *in* the
+# container, and have already installed git and pulled down this
+# repo.
+# Also note that this is specific to Fedora-flavored containers.
+#
+
+dnf install -y rpm-build rpm-devel rpmdevtools vim epel-release
+dnf install -y 'dnf-command(config-manager)'
+dnf config-manager --set-enabled epel
+dnf config-manager --set-enabled powertools
+# Now get the dependencies from the spec file
+dnf builddep -y ./swift-lang.spec
+
+echo Container is ready to gooooooo....
