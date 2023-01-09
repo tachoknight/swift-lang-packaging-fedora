e45ef5ae20a64de3f211455b2e3eab90ba8dcfc9
Ron Olson <tachoknight@gmail.com>
2023-01-09 12:25:39 -0600

    Changes to make container setup work with fedora

--- setup-container.sh
+++ setup-container.sh
@@ -11,16 +11,24 @@
 FLAVOR=`( lsb_release -ds || cat /etc/*release || uname -om ) 2>/dev/null | head -n1`
 echo Working with $FLAVOR
 
-dnf install -y rpm-build rpm-devel rpmdevtools vim epel-release
+dnf install -y rpm-build rpm-devel rpmdevtools vim 
 dnf install -y 'dnf-command(config-manager)'
 
-# Oracle does it differently
 if [[ $FLAVOR == "Oracle"* ]]; then
-	dnf config-manager --set-enabled ol8_codeready_builder
+	# Oracle does it differently
+        dnf install -y epel-release
+        dnf config-manager --set-enabled ol8_codeready_builder
+elif [[ $FLAVOR == "Fedora"* ]]; then
+	# Good ol' Fedora
+        echo Oh, working with Fedora are we?
 else
-	dnf config-manager --set-enabled epel
-	dnf config-manager --set-enabled powertools
+	# This should cover all the other flavors, like CentOS,
+	# Rocky, AlmaLinux, etc.
+        dnf install -y epel-release
+        dnf config-manager --set-enabled epel
+        dnf config-manager --set-enabled powertools
 fi
+
 
 # Now get the dependencies from the spec file
 dnf builddep -y ./swift-lang.spec
