eca2caa4a7f4c77c9334e04724b0b60edc24cad6
Ron Olson <tachoknight@gmail.com>
2023-01-17 10:48:30 -0600

    Added script to submit to koji

---
+++ submit-scratch-builds.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+#
+# The purpose of this script is to build the SRPM
+# and then send it to Koji for scratch builds so
+# all the various flavors can be tested to see if
+# they build correctly
+#
+
+export MYDIR=$PWD
+# Our current version of Fedora
+export FEDORA_VERSION=`rpm -E %fedora`
+
+echo Gonna submit scratch builds | figlet -c -f pagga | lolcat
+echo DID YOU LOG INTO KOJI FIRST? | figlet -c -f future | lolcat
+echo Building SRPM first...
+
+rm -rf $HOME/rpmbuild
+rm $MYDIR/build-output.txt
+mkdir -p $HOME/rpmbuild/{BUILD,RPMS,SOURCES,SPECS,SRPMS}
+cp $MYDIR/*.patch $HOME/rpmbuild/SOURCES
+cp $MYDIR/swift-lang.spec $HOME/rpmbuild/SPECS
+
+pushd $HOME/rpmbuild/SPECS
+spectool -g -R ./swift-lang.spec
+# Now do the actual build
+rpmbuild -bs ./swift-lang.spec 2>&1 | tee $MYDIR/build-output.txt
+
+# Now submit them
+echo Submitting to Koji | figlet -c -f future | lolcat
+export SRPM_FILE=`find $HOME/rpmbuild/SRPMS -name swift-lang\*`
+nohup koji build --scratch rawhide $SRPM_FILE >/dev/null &
+nohup koji build --scratch f$FEDORA_VERSION $SRPM_FILE >/dev/null &
+# Now the previous version of Fedora
+let "FEDORA_VERSION -= 1"
+nohup koji build --scratch f$FEDORA_VERSION $SRPM_FILE >/dev/null &
+# And the EPEL versions...
+nohup koji build --scratch epel9 $SRPM_FILE >/dev/null &
+nohup koji build --scratch epel8 $SRPM_FILE >/dev/null &
+
+popd
+
+echo Started:_____$START_TS
+echo Ended:_______`date`
