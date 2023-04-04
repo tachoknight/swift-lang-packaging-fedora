1484651ef426e73ad615ab41664897312fb17feb
Ron Olson <tachoknight@gmail.com>
2023-04-04 05:16:32 -0500

    Updated to 5.9-DEVELOPMENT-SNAPSHOT-2023-04-03-a

--- justbuild-mock.sh
+++ justbuild-mock.sh
@@ -4,8 +4,8 @@ MYDIR=$PWD
 
 START_TS=`date`
 
-#BUILD=fedora-rawhide-`uname -i`
-BUILD=alma+epel-8-`uname -m`
+BUILD=fedora-rawhide-`uname -i`
+#BUILD=alma+epel-8-`uname -m`
 #BUILD=fedora-36-`uname -i`
 
 rm -rf $HOME/rpmbuild
