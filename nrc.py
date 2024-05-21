802d138d3a87521551b5d3351805aeb667512e01
Ron Olson <tachoknight@gmail.com>
2024-05-21 13:36:28 -0500

    Updated to Swift 6

--- nrc.py
+++ nrc.py
@@ -7,7 +7,7 @@ import requests
 from bs4 import BeautifulSoup
 
 # The version we're autobuilding
-CURRENT_VERSION = 'swift-5.10'
+CURRENT_VERSION = 'swift-6.0'
 
 def left(s, amount):
     return s[:amount]
