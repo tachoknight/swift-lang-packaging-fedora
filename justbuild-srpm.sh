35e4f2ddd1c57a047abe4eebacf2ab4a9e8acbe2
Ron Olson <tachoknight@gmail.com>
2024-07-19 07:49:58 -0500

    Dunno why i commented out copying conf files

--- justbuild-srpm.sh
+++ justbuild-srpm.sh
@@ -15,7 +15,7 @@ rm -rf $HOME/rpmbuild
 rm $MYDIR/build-output.txt
 mkdir -p $HOME/rpmbuild/{BUILD,RPMS,SOURCES,SPECS,SRPMS}
 cp $PWD/*.patch $HOME/rpmbuild/SOURCES
-#cp $PWD/*.conf $HOME/rpmbuild/SOURCES
+cp $PWD/*.conf $HOME/rpmbuild/SOURCES
 cp $PWD/swift-lang.spec $HOME/rpmbuild/SPECS
 
 pushd $HOME/rpmbuild/SPECS
